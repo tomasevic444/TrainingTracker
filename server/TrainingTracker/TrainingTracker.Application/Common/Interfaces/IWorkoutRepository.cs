dd13fcec3b6fd81ad6430a6a00d3a944bd311fa7
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-03 23:45:29 +0200

    feat(workouts): Add workout repository for data access

---
+++ server/TrainingTracker/TrainingTracker.Application/Common/Interfaces/IWorkoutRepository.cs
@@ -0,0 +1,9 @@
+﻿using TrainingTracker.Core.Entities;
+
+namespace TrainingTracker.Application.Common.Interfaces;
+
+public interface IWorkoutRepository
+{
+    void Add(Workout workout);
+    Task<bool> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ server/TrainingTracker/TrainingTracker.Infrastructure/Persistence/Repositories/WorkoutRepository.cs
@@ -0,0 +1,24 @@
+﻿using TrainingTracker.Application.Common.Interfaces;
+using TrainingTracker.Core.Entities;
+
+namespace TrainingTracker.Infrastructure.Persistence.Repositories;
+
+public class WorkoutRepository : IWorkoutRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public WorkoutRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(Workout workout)
+    {
+        _context.Workouts.Add(workout);
+    }
+
+    public async Task<bool> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await _context.SaveChangesAsync(cancellationToken) > 0;
+    }
+}
