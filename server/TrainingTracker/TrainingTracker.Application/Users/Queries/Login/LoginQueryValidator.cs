614a39a4e8d209fd7f40f5e9e88ef50df8e60dfe
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-03 19:47:07 +0200

    feat(auth): Add validation for login query

---
+++ server/TrainingTracker/TrainingTracker.Application/Users/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TrainingTracker.Application.Users.Queries.Login;
+
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+    public LoginQueryValidator()
+    {
+        RuleFor(x => x.LoginIdentifier)
+            .NotEmpty().WithMessage("Username or email is required.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.");
+    }
+}
