033920ada6f3f88d2f36b46555b83ca6d8c286ba
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-03 13:53:06 +0200

    feat(core): Define User and Workout entities

---
+++ server/TrainingTracker/TrainingTracker.Core/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainingTracker.Core.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public ICollection<Workout> Workouts { get; set; } = new List<Workout>();
+}
---
+++ server/TrainingTracker/TrainingTracker.Core/Entities/Workout.cs
@@ -0,0 +1,15 @@
+﻿namespace TrainingTracker.Core.Entities;
+
+public class Workout
+{
+    public Guid Id { get; set; }
+    public required string ExerciseType { get; set; }
+    public int DurationInMinutes { get; set; }
+    public int CaloriesBurned { get; set; }
+    public int Intensity { get; set; } // 1-10
+    public int Fatigue { get; set; } // 1-10
+    public string? Notes { get; set; }
+    public DateTime Date { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+}
