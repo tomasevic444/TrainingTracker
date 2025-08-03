4aa97aff2e64bf188f46f6d2c6eacb6eeb73d7b0
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-03 14:28:55 +0200

    feat(users): Implement user registration command and handler

---
+++ server/TrainingTracker/TrainingTracker.Application/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace TrainingTracker.Application.Users.Commands.RegisterUser;
+
+// IRequest<Guid> means that executing this command will return a Guid (the ID of the new user)
+public class RegisterUserCommand : IRequest<Guid>
+{
+    public required string Username { get; set; } 
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
---
+++ server/TrainingTracker/TrainingTracker.Application/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using TrainingTracker.Application.Users.Commands.RegisterUser;
+using TrainingTracker.Core.Entities;
+
+public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Guid>
+{
+    private readonly IUserRepository _userRepository;
+
+    public RegisterUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        if (await _userRepository.UserExistsAsync(request.Email, cancellationToken))
+        {
+            throw new Exception("User with this email already exists.");
+        }
+
+        if (await _userRepository.UsernameExistsAsync(request.Username, cancellationToken))
+        {
+            throw new Exception("This username is already taken.");
+        }
+
+        var passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Username = request.Username, 
+            Email = request.Email,
+            PasswordHash = passwordHash
+        };
+
+        _userRepository.Add(user);
+        await _userRepository.SaveChangesAsync(cancellationToken);
+
+        return user.Id;
+    }
+}
