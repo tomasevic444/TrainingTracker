f88eb380d61e1033e400d5ea259494f18cb29d5b
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-03 23:44:48 +0200

    feat(workouts): Add command and validator for creating workouts

---
+++ server/TrainingTracker/TrainingTracker.Application/Workouts/Commands/CreateWorkout/CreateWorkoutCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace TrainingTracker.Application.Workouts.Commands.CreateWorkout;
+
+// This command will return the Guid of the newly created workout
+public class CreateWorkoutCommand : IRequest<Guid>
+{
+    // We don't need UserId here. We'll get it from the authenticated user's token.
+    public required string ExerciseType { get; set; }
+    public int DurationInMinutes { get; set; }
+    public int CaloriesBurned { get; set; }
+    public int Intensity { get; set; } // 1-10
+    public int Fatigue { get; set; }   // 1-10
+    public string? Notes { get; set; }
+    public DateTime Date { get; set; }
+}
---
+++ server/TrainingTracker/TrainingTracker.Application/Workouts/Commands/CreateWorkout/CreateWorkoutCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace TrainingTracker.Application.Workouts.Commands.CreateWorkout;
+
+public class CreateWorkoutCommandValidator : AbstractValidator<CreateWorkoutCommand>
+{
+    public CreateWorkoutCommandValidator()
+    {
+        RuleFor(x => x.ExerciseType).NotEmpty();
+
+        RuleFor(x => x.DurationInMinutes)
+            .GreaterThan(0).WithMessage("Duration must be greater than 0.");
+
+        RuleFor(x => x.CaloriesBurned)
+            .GreaterThanOrEqualTo(0).WithMessage("Calories burned cannot be negative.");
+
+        RuleFor(x => x.Intensity)
+            .InclusiveBetween(1, 10).WithMessage("Intensity must be between 1 and 10.");
+
+        RuleFor(x => x.Fatigue)
+            .InclusiveBetween(1, 10).WithMessage("Fatigue must be between 1 and 10.");
+
+        RuleFor(x => x.Date)
+            .NotEmpty()
+            .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Workout date cannot be in the future.");
+    }
+}
