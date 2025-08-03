0d70a1c39f66c1c1396dd4d6e2df04016d75cfe1
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-03 14:29:18 +0200

    feat(users): Add user repository for data access

---
+++ server/TrainingTracker/TrainingTracker.Application/Common/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using TrainingTracker.Core.Entities;
+
+public interface IUserRepository
+{
+    Task<bool> UserExistsAsync(string email, CancellationToken cancellationToken);
+    Task<bool> UsernameExistsAsync(string username, CancellationToken cancellationToken);
+    void Add(User user);
+    Task<bool> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ server/TrainingTracker/TrainingTracker.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingTracker.Core.Entities;
+
+namespace TrainingTracker.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> UserExistsAsync(string email, CancellationToken cancellationToken)
+    {
+        return await _context.Users.AnyAsync(u => u.Email == email, cancellationToken);
+    }
+
+    public void Add(User user)
+    {
+        _context.Users.Add(user);
+    }
+
+    public async Task<bool> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await _context.SaveChangesAsync(cancellationToken) > 0;
+    }
+    public async Task<bool> UsernameExistsAsync(string username, CancellationToken cancellationToken)
+    {
+        return await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower(), cancellationToken);
+    }
+}
