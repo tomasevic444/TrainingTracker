070c48638aea759721301897916400df7835ef22
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-04 01:08:27 +0200

    feat(workouts): Define DTO and query for getting user workouts

---
+++ server/TrainingTracker/TrainingTracker.Application/Workouts/Queries/GetUserWorkouts/GetUserWorkoutsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TrainingTracker.Application.Workouts.Queries.GetUserWorkouts;
+
+// This query will return a list of our DTOs
+public class GetUserWorkoutsQuery : IRequest<IEnumerable<WorkoutDto>>
+{
+    // This query doesn't need any parameters, because the user ID
+    // will be retrieved from the user context.
+}
---
+++ server/TrainingTracker/TrainingTracker.Application/Workouts/Queries/GetUserWorkouts/WorkoutDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainingTracker.Application.Workouts.Queries.GetUserWorkouts;
+
+public class WorkoutDto
+{
+    public Guid Id { get; set; }
+    public required string ExerciseType { get; set; }
+    public int DurationInMinutes { get; set; }
+    public int CaloriesBurned { get; set; }
+    public int Intensity { get; set; }
+    public int Fatigue { get; set; }
+    public string? Notes { get; set; }
+    public DateTime Date { get; set; }
+}
