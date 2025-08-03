af12634bcfd551f942a2735e662afe133c091584
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-03 23:46:17 +0200

    feat(auth): Add user context service to get current user ID

---
+++ server/TrainingTracker/TrainingTracker.API/Services/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using TrainingTracker.Application.Common.Interfaces;
+
+namespace TrainingTracker.API.Services;
+
+public class UserContext : IUserContext
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UserContext(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid? GetUserId()
+    {
+        var userIdClaim = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (Guid.TryParse(userIdClaim, out var userId))
+        {
+            return userId;
+        }
+
+        return null;
+    }
+}
---
+++ server/TrainingTracker/TrainingTracker.Application/Common/Interfaces/IUserContext.cs
@@ -0,0 +1,6 @@
+﻿namespace TrainingTracker.Application.Common.Interfaces;
+
+public interface IUserContext
+{
+    Guid? GetUserId(); // The ID might be null if the user is not authenticated
+}
