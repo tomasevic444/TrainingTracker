9a80a784e3493f5814e70f0d258bc04ac2ad5b0b
Nikola Tomasevic <tomasevic444@gmail.com>
2025-08-04 18:26:31 +0200

    feat(workouts): Define query and DTOs for weekly summary

---
+++ server/TrainingTracker/TrainingTracker.Application/Workouts/Queries/Queries/GetWeeklySummary/GetWeeklySummaryQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TrainingTracker.Application.Workouts.Queries.GetWeeklySummary;
+
+// This query takes a year and month and returns a list of weekly summaries
+public class GetWeeklySummaryQuery : IRequest<IEnumerable<WeeklySummaryDto>>
+{
+    public int Year { get; set; }
+    public int Month { get; set; }
+}
---
+++ server/TrainingTracker/TrainingTracker.Application/Workouts/Queries/Queries/GetWeeklySummary/WeeklySummaryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainingTracker.Application.Workouts.Queries.GetWeeklySummary;
+
+// This will hold the stats for a single week
+public class WeeklySummaryDto
+{
+    public int WeekNumber { get; set; }
+    public DateTime WeekStartDate { get; set; }
+    public DateTime WeekEndDate { get; set; }
+    public int TotalDurationInMinutes { get; set; }
+    public int TotalWorkouts { get; set; }
+    public double AverageIntensity { get; set; }
+    public double AverageFatigue { get; set; }
+}
